3ee67da0ff792c5549c5b91cf0d137e332b65700
Andres D' Amelio <andresdamelio@gmail.com>
2020-04-12 23:58:58 -0400

    Proyecto de presupuesto, creando primero componente para recibir la infromacion, y agregando el useState para la cantidad y para la validacion de errores

---
+++ src/components/Pregunta.js
@@ -0,0 +1,45 @@
+import React, { Fragment, useState } from 'react';
+
+const Pregunta = () => {
+
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    // Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value,10));
+    }
+
+    // submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+
+        // Si se pasa de la validacion
+    }
+
+    // Validar formulario
+
+    return (  
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+            <form onSubmit={agregarPresupuesto}>
+                <input 
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir presupuesto"
+                />
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default Pregunta;
